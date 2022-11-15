8190d1d1b9edc04f23912ce76328c20cf919de45
Gary Giambatista <gary.giambatista@gmail.com>
2022-11-15 03:47:54 -0500

    fixed 1080 res and added on hover greyscale and scaleing.

---
+++ _next/static/g5NfWxafWiIFtMd5nSbho/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/488-b7f53f1486a48dc8.js","static/chunks/pages/index-782c98945554a544.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
